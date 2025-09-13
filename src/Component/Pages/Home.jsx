2f32bae58dec0be76017471c7fb9f406566ba1c4
SherSinghDev <shersingh741730@gmail.com>
2025-09-13 16:22:48 +0530

    images

--- src/Component/Pages/Home.jsx
+++ src/Component/Pages/Home.jsx
@@ -1,4 +1,4 @@
-import heroImg from "../../assets/my_image.jpg"
+import heroImg from "../../assets/teamone.jpeg"
 import { ReactTyped } from "react-typed";
 import { open } from "../../store/slices/sidebarSlice"
 import { useDispatch, useSelector } from "react-redux";
@@ -22,7 +22,7 @@ export default function Home() {
                         <i className="fa-solid fa-bars"></i>
                     </div>
                     <div className="main-content">
-                        <h1 className="main-head">Hello, my name is <span className="">Sher Singh</span></h1>
+                        <h1 className="main-head">Hello, my name is <span className="clicker">Sher Singh</span></h1>
                         <h1 className="main-head">I'm a <ReactTyped strings={["MERN Developer", "Backend Developer", "Frontend Developer"]} typeSpeed={80} backSpeed={40} loop /></h1>
                         <p className="para">As a full-stack web developer, my passion lies in the seamless integration of front-end and back-end technologies to build cohesive and dynamic web applications. I am driven by the challenge of bridging the gap between user experience and functionality, ensuring that every application I develop is both visually appealing and highly performant.</p>
                         <Link to="/about"><button className="btn1">More About Me</button></Link>
